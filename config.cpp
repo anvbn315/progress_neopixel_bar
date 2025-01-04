f0f285ff468da4b23d19dc4a7c15cbab75c43b68
anvbn315 <115771229+anvbn315@users.noreply.github.com>
2025-01-04 14:26:44 +0700

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,8 +1,8 @@
 #include "config.h"
 
-const char* ssid = "Thanh Tu";  
-const char* password = "44010783";  
-const char* ip_host = "192.168.1.252";
+const char* ssid = "...";  
+const char* password = "...";  
+const char* ip_host = "...";
 const uint16_t port = 3000;  
 
 const int LED = 2;  
